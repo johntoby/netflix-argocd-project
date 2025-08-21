f9d5c090eaf36bfa975585b3d19fb1e96523b9cb
johntoby <johntoby007@gmail.com>
2025-08-21 15:32:42 +0100

    update to main.tf file

--- terraform/main.tf
+++ terraform/main.tf
@@ -21,10 +21,12 @@ provider "aws" {
 }
 
 data "aws_eks_cluster" "cluster" {
+  depends_on = [module.eks]
   name = module.eks.cluster_name
 }
 
 data "aws_eks_cluster_auth" "cluster" {
+  depends_on = [module.eks]
   name = module.eks.cluster_name
 }
 
